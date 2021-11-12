90d44af178c3cc2ed183810d41e2e86557169421
Rujuta Dabke <89774970+RujutaDabke@users.noreply.github.com>
2021-11-12 20:34:12 +0530

    Add files via upload

---
+++ Prototype/src/refactoring_guru/prototype/example/shapes/Main.java
@@ -0,0 +1,122 @@
+package refactoring_guru.prototype.example.shapes;
+
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Main {
+    public int x;
+    public int y;
+    public String color;
+
+    public Main() {
+    }
+    public Main(Main target) {
+        if (target != null) {
+            this.setX(target.getX());
+            this.y = target.y;
+            this.color = target.color;
+        }
+    }
+    public abstract Main clone();
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Main)) return false;
+        Main shape2 = (Main) object2;
+        return shape2.getX() == getX() && shape2.y == y && Objects.equals(shape2.color, color);
+    }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+class Circle extends Main {
+    public int radius;
+    public Circle() {
+    }
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+    @Override
+    public Main clone() {
+        return new Circle(this);
+    }
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle) || !super.equals(object2)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
+}
+class Rectangle extends Main {
+    public int width;
+    public int height;
+    public Rectangle() {
+    }
+    public Rectangle(Rectangle target) {
+        super(target);
+        if (target != null) {
+            this.width = target.width;
+            this.height = target.height;
+        }
+    }
+    @Override
+    public Main clone() {
+        return new Rectangle(this);
+    }
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Rectangle) || !super.equals(object2)) return false;
+        Rectangle shape2 = (Rectangle) object2;
+        return shape2.width == width && shape2.height == height;
+    }
+}
+
+
+class Demo {
+    public static void main(String[] args) {
+        List<Main> shapes = new ArrayList<>();
+        List<Main> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+    private static void cloneAndCompare(List<Main> shapes, List<Main> shapesCopy) {
+        for (Main shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
